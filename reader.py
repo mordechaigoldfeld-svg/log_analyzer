a5d39c78a1cbd50e0508ee77ec456f55edf12cff
moty <g38257361@gmail.com>
2026-02-11 16:30:00 +0200

    aded_func

--- reader.py
+++ reader.py
@@ -1,45 +1,51 @@
 import csv
 
-
 def reader_csv_file(file_path):
-    list_file=[]
     with open(file_path) as csv_file:
         csv_reader = csv.reader(csv_file)
-        for row in csv_reader:
-            list_file.append(row)
-    return list_file
+        return [row for row in csv_reader]
 
 file_path='network_traffic.log'
 data=reader_csv_file(file_path)
 # for row in data:
 #     print(row)
 
+
+
 def external_ip(data):
-    external_ip_list = []
-    for row in data:
-        ip=row[1]
-        if not ip.startswith('192') and not ip.startswith('10.'):
-            external_ip_list.append(row)
-    return external_ip_list
+    return[row for row in data
+        if not row[1].startswith('192') and not row[1].startswith('10.')
+    ]
 
 # external_ip_list=external_ip(data)
 # for row in external_ip_list[ :50]:
 #     print(row)
 
-
-
-
-
 def sensitive_port(data):
-    sensitive_port_lst = []
-    for row in data:
-        port = row[3]
-        if port=='22' or port=='23' or port=='3389':
-            sensitive_port_lst.append(row)
-    return sensitive_port_lst
+    return[row
+           for row in data
+           if row[3]=='22' or row[3]=='23' or row[3]=='3389'
+           ]
 
-sensitive_port_list=sensitive_port(data)
-for row in sensitive_port_list:
+
+# sensitive_port_list=sensitive_port(data)
+# for row in sensitive_port_list:
+#     print(row)
+
+
+def big_size(data):
+    return[row
+           for row in data
+           if int(row[5]) >5000]
+# big_bite=big_size(data)
+# for row in big_bite[:50]:
+#     print(row)
+
+def size_status(data):
+    return[row+['large']  if int(row[5]) > 5000 else row+['normal']
+           for row in data]
+
+size_update=size_status(data)
+for row in size_update:
     print(row)
-
 
